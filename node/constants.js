571ef0aece353440411b6c3c5fed3bbb7781e800
Liz Howard <liz@enki.com>
2018-01-06 23:23:37 -0800

    restructure for use as package

--- node/constants.js
+++
@@ -1,3 +0,0 @@
-exports.CURRICULUM_BASE_URL = 'https://github.com/enkidevs/curriculum';
-
-exports.STANDARDS_BASE_URL = 'https://github.com/sagelabs/standards';
--- node/index.js
+++
@@ -1,9 +0,0 @@
-const CONSTANTS = require("./constants");
-const helpers = require("./helpers");
-const content = require("./lib");
-
-module.exports = {
-  ...CONSTANTS,
-  ...helpers,
-  content
-}

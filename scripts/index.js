6231b53137c97c5be117f7860fd5d5ae25d03b1b
Miles <miles.b.flo@gmail.com>
2018-01-22 14:22:34 -0800

    Created library methods and cleaned up scripts

--- scripts/index.js
+++ scripts/index.js
@@ -12,35 +12,3 @@ const curriculum = new Curriculum(git);
 const fs = require('fs');
 const path = require('path');
 let curriculumStats = curriculum.getStats();
-
-
-// for (let t in curriculum.topics) {
-//   let topic = curriculum.topics[t];
-//   if (topic.courses) {
-//     for (let c in topic.courses) {
-//       let course = topic.courses[c];
-//       course.writeFile()
-//     }
-//   }
-// }
-
-/**
- * Writes Course pages for wiki in `curriculum.wiki` local directory
- *
- */
-function writeCoursePages() {
-  for (let i in curriculum.topics) {
-    let data = "";
-    data += `- [Content Folder](${remote}${i})
-- [Standards](${remote}${i})
-
-## Live Courses
-`;
-    let topic = curriculum.topics[i];
-    for (j in topic.courses) {
-      data+=`- [${ toTitleCase( j.replace(/\-/g," ") ) }](${remote}${i}/${j})\n`;
-    }
-    fs.writeFileSync(path.join(basePath, `curriculum.wiki/${toTitleCase(i)}-Topic.md`), data);
-  }
-
-}

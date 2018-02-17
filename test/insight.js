bea7d7ce27d2418ecf3ac648b6f9dc95830d04e7
Liz Howard <liz@enki.com>
2018-02-17 14:30:57 -0600

    add several insight fixtures, add two tests to insight to get everyone started

--- test/insight.js
+++ test/insight.js
@@ -1,5 +1,54 @@
-const assert = require('assert');
-// 
-// describe('Insight', () => {
-//   describe('')
-// });
+const expect = require('chai').expect;
+const Insight = require("../lib").Insight;
+const fs = require("fs");
+
+const testInsights = [];
+
+describe('Insight', () => {
+  let fixtures = [];
+  before(function() {
+    let files = fs.readdirSync("test/fixtures/insights");
+    for (let filePath of files) {
+      fixtures.push(`test/fixtures/insights/${filePath}`);
+    }
+    for (let i in fixtures) {
+      let body = fs.readFileSync(fixtures[i]).toString();
+      testInsights.push(new Insight({body, path: fixtures[i]}))
+    }
+  })
+  it("Parses the title from the raw insight text", function(){
+
+    testInsights.forEach((insight)=>{
+      expect(insight).to.haveOwnProperty('title')
+    });
+
+    let titles = testInsights.map((insight) => {return insight.title});
+
+    expect(titles).to.have.members([
+      'Declaring Functions',
+      'Immediately-Invoked Function Expression (IIFE)',
+      'Multiple JOINs',
+      'UNION',
+      'Using `curl` To Make HTTP Requests',
+      'Using `ping` To Measure Network Latency'
+    ]);
+  })
+
+  it("Parses the slug from the filename", function(){
+
+    testInsights.forEach((insight)=>{
+      expect(insight).to.haveOwnProperty('slug')
+    });
+
+    let slugs = testInsights.map((insight) => {return insight.slug});
+
+    expect(slugs).to.have.members([
+      'multiple-joins',
+      'union',
+      'using-curl-to-make-http-requests',
+      'using-ping-to-measure-network-latency',
+      'declaring-functions',
+      'immediately-invoked-function-expression-iife'
+    ]);
+  })
+});

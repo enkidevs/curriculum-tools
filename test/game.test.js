d36786b5fe8fc1040972747c030c3cf20237e766
Miles <miles.b.flo@gmail.com>
2018-02-26 15:36:21 -0800

    Adding tetris fixture and test suite

---
+++ test/game.test.js
@@ -0,0 +1,22 @@
+const Game = require('../lib').Game;
+const fs = require('fs');
+const path = require('path');
+
+const gamesPath = 'fixtures/games';
+
+let testGames = [];
+
+// Preflight
+for (let file of fs.readdirSync(path.join(__dirname, gamesPath))) {
+    let contentPath = path.join(__dirname, `${gamesPath}/${file}`);
+    let body = fs.readFileSync(contentPath, 'utf8');
+    testGames.push(new Game({ body, path : contentPath }));
+}
+// console.log(testGames[0])
+for (let game of testGames) {
+    test(`Game: ${game.title}`, function() {
+        expect(game).toHaveProperty("rawText");
+        expect(game).toHaveProperty("contentPath");
+        expect(game).toHaveProperty("slug");
+    })
+}

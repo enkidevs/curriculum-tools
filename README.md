dd4622f04f6ffbc677fa8a794d475554057ab442
Liz Howard <liz@enki.com>
2018-03-12 10:27:05 -0700

    hacky, non-new-parser version of ct to rerender curriculum to new format

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # curriculum-tools
 Hacky scripts for organizing, analysing, and reformatting curriculum programmatically. GPL3
+
+
+
+
+todo
+- play with unified, go through the guides
+- translate quizzes to normal question format, headline is an h3
+- global headline under yaml
+- yaml must be contained within ---, ---
+- integrate curriclum parser
+- write the cli api
---
+++ scripts/rerenderCurriculum.js
@@ -0,0 +1,20 @@
+#! usr/bin/env node
+
+const Curriculum = require('../lib/curriculum');
+const GitHub = require('../lib/networking/github');
+const fs = require('fs');
+
+const basePath = process.argv[2];
+
+const remote = "https://github.com/enkidevs/curriculum/tree/master/";
+const git = new GitHub(basePath);
+
+const curriculum = new Curriculum(git);
+
+// from here you can script changes to the curriculum
+
+let allInsights = curriculum.getAllInsights().filter((insight) => { if (insight.contentPath) return insight.contentPath.indexOf("getting-started/some-common-terminology.md") !== -1} )
+
+allInsights.forEach((insight) => {
+    fs.writeFileSync(insight.contentPath, insight.render())
+})

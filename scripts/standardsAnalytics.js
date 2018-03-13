622628bf0473db54d164f5f2d773d5daa9279b81
Liz Howard <liz@enki.com>
2018-03-13 15:16:38 -0700

    add fix from other branch

--- scripts/standardsAnalytics.js
+++ scripts/standardsAnalytics.js
@@ -1,60 +1,62 @@
 #! usr/bin/env node
 
-const Curriculum = require('../lib/curriculum');
-const GitHub = require('../lib/networking/github');
+const Curriculum = require('../lib/curriculum')
+const GitHub = require('../lib/networking/github')
 
-const basePath = process.argv[2];
+const basePath = process.argv[2]
 
-const git = new GitHub(basePath);
+const git = new GitHub(basePath)
 
-const curriculum = new Curriculum(git);
+const curriculum = new Curriculum(git)
 
 // from here you can script changes to the curriculum
 
 let courses = curriculum.getAllCourses()
 let insights = curriculum.getAllInsights()
 
-let standards = [];
-let standardsBySlug = {};
-let objectives = new Set();
-let objectivesBySlug = {};
+let standards = []
+let standardsBySlug = {}
+let objectives = new Set()
+let objectivesBySlug = {}
 
-let coveredObjectives = new Set();
-let uncoveredObjectives = [];
-let mistaggedObjectives = [];
+let coveredObjectives = new Set()
+let uncoveredObjectives = []
+let mistaggedObjectives = []
 
-courses.forEach(function(course) {
-    course.standards.forEach((standard) => {
-        standards.push(standard);
-        standardsBySlug[standard.slug] = standard;
-        standard.objectives.forEach((objective, i) => {
-            objectives.add(objective)
-            objectivesBySlug[`${standard.slug}.${i}`] = objective
-        })
+courses.forEach(function (course) {
+  course.standards.forEach((standard) => {
+    standards.push(standard)
+    standardsBySlug[standard.slug] = standard
+    standard.objectives.forEach((objective, i) => {
+      objectives.add(objective)
+      objectivesBySlug[`${standard.slug}.${i}`] = objective
     })
+  })
 })
 
-
 insights.forEach((insight) => {
-    if (insight.standards) {
-        let standardsTags = Object.keys(insight.standards);
-        standardsTags.forEach((tag) => {
-            if (objectivesBySlug[tag]) {
-                coveredObjectives.add(tag)
-            } else {
-                mistaggedObjectives.push({
-                    tag,
-                    insight
-                })
-            }
+  if (insight.standards) {
+    let standardsTags = Object.keys(insight.standards)
+    standardsTags.forEach((tag) => {
+      if (objectivesBySlug[tag]) {
+        coveredObjectives.add(tag)
+      } else {
+        mistaggedObjectives.push({
+          tag,
+          insight
         })
-    }
+      }
+    })
+  }
 })
 
 objectives.forEach((objective) => {
-    if (!coveredObjectives.has(objective)) {
-        uncoveredObjectives.push(objective)
-    }
+  if (!coveredObjectives.has(objective)) {
+    uncoveredObjectives.push(objective)
+  }
 })
 
+console.log("Covered Objectives")
+console.log(coveredObjectives)
+console.log("Uncovered Objectives")
 console.log(uncoveredObjectives)

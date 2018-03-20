861455c125a0be965ece61b42ffbf9f483998da7
Miles <miles.b.flo@gmail.com>
2018-03-20 11:45:37 -0700

    Insight to export curriculum

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
-#! usr/bin/env node
+#!/usr/bin/env node
 
-const Curriculum = require('../lib/curriculum');
-const GitHub = require('../lib/networking/github');
+const Curriculum = require('../lib/curriculum')
+const GitHub = require('../lib/networking/github')
 
-const basePath = process.argv[2];
+const basePath = process.argv[2]
 
-const remote = "https://github.com/enkidevs/curriculum/tree/master/";
-const git = new GitHub(basePath);
+const git = new GitHub(basePath)
 
-const curriculum = new Curriculum(git);
+const curriculum = new Curriculum(git)
 
 // from here you can script changes to the curriculum.cl
+
+module.exports = curriculum

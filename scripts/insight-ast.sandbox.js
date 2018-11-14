f69caeaad9781628cd9f849ba90d60c11eb22688
Miles <14067660+milesflo@users.noreply.github.com>
2018-11-14 13:24:57 -0800

    Adding a useful sandboxing script

--- scripts/insight-ast.sandbox.js
+++ scripts/insight-ast.sandbox.js
@@ -1,11 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 const { Insight } = require('../lib')
-const insightDataPath = path.join(__dirname, '../test/fixtures/insights/introducing-the-osi-model.md')
+const insightDataPath = path.join(__dirname, '../test/fixtures/insights/successful-status-codes.md')
 const insightData = fs.readFileSync(insightDataPath, 'utf8')
 
 const insight = new Insight({body: insightData, path: insightDataPath})
 
-// console.log(insight.ast.children[0].data.parsedValue)
-insight.setContent('asdf')
 console.log(insight.getContent())

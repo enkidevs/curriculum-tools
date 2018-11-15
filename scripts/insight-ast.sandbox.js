65f0ca3181216bb83749d6aeba685a8fbf589c0e
Miles <14067660+milesflo@users.noreply.github.com>
2018-11-14 16:05:19 -0800

    Including all fixture titles in test suite

--- scripts/insight-ast.sandbox.js
+++ scripts/insight-ast.sandbox.js
@@ -6,4 +6,4 @@ const insightData = fs.readFileSync(insightDataPath, 'utf8')
 
 const insight = new Insight({body: insightData, path: insightDataPath})
 
-console.log(insight.getPracticeQuestion())
+console.log(insight.getPracticeQuestion().text)
--- test/fixtures/insights/functional-components.md
+++ test/fixtures/insights/functional-components.md
@@ -25,7 +25,7 @@ links:
 
 ---
 
-# Functional components
+# Functional Components
 
 ---
 ## Content
